fb5de3057c14ddf5a1da0fa08ea41c918ff8809f
LUNA\myhgh <myhghjjang@naver.com>
2022-06-14 17:56:52 +0900

    Initial commit

---
+++ myhgh/myhgh/src/main/java/com/example/myhgh/MyhghApplication.java
@@ -0,0 +1,13 @@
+package com.example.myhgh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyhghApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyhghApplication.class, args);
+	}
+
+}
---
+++ myhgh/myhgh/src/test/java/com/example/myhgh/MyhghApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myhgh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyhghApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
